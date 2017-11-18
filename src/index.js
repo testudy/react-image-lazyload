e005c1aec18de403bc51b61ffb7b2f235f0425e9
Hu Jiwei <hu_jiwei@foxmail.com>
2017-11-18 12:23:09 +0800

    添加基本js和css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import './index.css';
+
 function Image(props) {
-    return <img {...props} />;
+    return (
+        <div role="img" src={"图片路径"} class="lazyload-img" style="width: 图片宽度px;">
+            <div style="padding: 图片高宽百分比% 0 0 0;">
+                <img {...props} data-lazyload-original="图片路径" data-lazyload-state="interactive|loading|complete|error" />
+            </div>
+        </div>
+    );
 }
 
 function lazy(component) {
