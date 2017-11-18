69ba99167fa8ed5c14dd7ef59f811e02bc3fe04b
Hu Jiwei <hu_jiwei@foxmail.com>
2017-11-18 12:23:57 +0800

    初步重构

--- src/lazyload.js
+++ src/lazyload.js
@@ -1,141 +1,133 @@
-define(function (require, exports, module) {
-    var $ = require('jquery'),
-        Stack = require('./stack'),
-        HashMap = require('./hashmap'),
-        Loader = require('./loader'),
-        global = window,
-        animationFrame = require('../../util/animation-frame/js/animation-frame'),
-        instance;
+import $ from 'jquery';
+import Stack from './stack';
+import HashMap from './hashmap';
+import Loader from './loader';
+import animationFrame from './animation-frame';
 
-    function Lazyload() {
-        var load = $.proxy(this.load, this);
 
-        this.$global = $(window);
-
+class Lazyload {
+    constructor () {
+        this.$window = $(window);
+ 
         this.hashmap = new HashMap();
         this.stack = new Stack();
         this.loader = new Loader({
-            success: load,
-            fail: load
+            success: this.load,
+            fail: this.load
         });
-
+ 
         this.listen();
-        this.update = $.proxy(this.update, this);
     }
 
-    Lazyload.prototype = {
-        constructor: Lazyload,
+    listen () {
+        this.$window.on('resize', this.request);
+        this.$window.on('scroll', this.request);
+    }
 
-        listen: function () {
-            var request = $.proxy(this.request, this);
-            this.$global.on('resize', request);
-            this.$global.on('scroll', request);
-        },
-
-        load: function () {
-            if (this.loader.isLoading || this.stack.isEmpty()) {
-                return;
-            }
-            this.loader.load(this.stack.pop());
-        },
-
-        init: function (element) {
-            var that = this,
-                $images = $(element).find('img[data-lazyload-original]');
-
-            $images.each(function () {
-                if (that.hashmap.containsKey(this)) {
-                    return;
-                }
-                if (this.getAttribute('data-lazyload-state')) {
-                    return;
-                }
-
-                var $this = $(this);
-                if (that.isVisible($this)) {
-                    that.interact(this);
-                    that.hashmap.put(this, {
-                        element: this,
-                        top: $this.offset().top,
-                        height: $this.innerHeight()
-                    });
-                }
-            });
-
-            this.update();
-        },
-
-        interact: function (element) {
-            element.setAttribute('data-lazyload-state', 'interactive');
-        },
-
-        request: function () {
-            if (this.requesting) {
-                return;
-            }
-            this.requesting = true;
-            animationFrame.request(this.update);
-        },
-
-        update: function () {
-            var that = this,
-                images = this.hashmap.values();
-
-            this.requesting = false;
-
-            if (images.length === 0) {
-                return;
-            }
-
-            $.each(images, function () {
-                if (that.inViewport(this.top, this.height)) {
-                    that.hashmap.remove(this.element);
-                    that.stack.push(this.element);
-                    that.load();
-                }
-            });
-        },
-
-        isVisible: function ($elem) {
-            return !!($elem.width() || $elem.height()) &&
-                $elem.css('display') !== 'none';
-        },
-
-        belowTheFold: function (top, height) {
-            var bottom = top + height + 200,
-                scrollY = global.scrollY || global.pageYOffset || global.document.documentElement.scrollTop,
-                fold;
-
-            fold = scrollY;
-
-            return fold <= bottom;
-        },
-
-        aboveTheFold: function (top, height) {
-            var fold,
-                scrollY = global.scrollY || global.pageYOffset || global.document.documentElement.scrollTop;
-
-            top -= 200;
-            fold = this.$global.height() + scrollY;
-
-            return fold >= top;
-        },
-
-        inViewport: function (top, height) {
-            return this.belowTheFold(top, height) &&
-                this.aboveTheFold(top, height);
+    load = () => {
+        if (this.loader.isLoading || this.stack.isEmpty()) {
+            return;
         }
+        this.loader.load(this.stack.pop());
     };
 
-    module.exports = function (element) {
-        element = element || window;
+    init (element) {
+        var that = this,
+            $images = $(element).find('img[data-lazyload-original]');
 
-        if (!instance) {
-            instance = new Lazyload();
+        $images.each(function () {
+            if (that.hashmap.containsKey(this)) {
+                return;
+            }
+            if (this.getAttribute('data-lazyload-state')) {
+                return;
+            }
+
+            var $this = $(this);
+            if (that.isVisible($this)) {
+                that.interact(this);
+                that.hashmap.put(this, {
+                    element: this,
+                    top: $this.offset().top,
+                    height: $this.innerHeight()
+                });
+            }
+        });
+
+        this.update();
+    }
+
+    interact (element) {
+        element.setAttribute('data-lazyload-state', 'interactive');
+    }
+
+    request = () => {
+        if (this.requesting) {
+            return;
+        }
+        this.requesting = true;
+        animationFrame.request(this.update);
+    };
+
+    update = () => {
+        var that = this,
+            images = this.hashmap.values();
+
+        this.requesting = false;
+
+        if (images.length === 0) {
+            return;
         }
 
-        instance.init(element);
-
-        return instance;
+        $.each(images, function () {
+            if (that.inViewport(this.top, this.height)) {
+                that.hashmap.remove(this.element);
+                that.stack.push(this.element);
+                that.load();
+            }
+        });
     };
-});
+
+    isVisible ($elem) {
+        return !!($elem.width() || $elem.height()) &&
+            $elem.css('display') !== 'none';
+    }
+
+    belowTheFold (top, height) {
+        var bottom = top + height + 200,
+            scrollY = window.scrollY || window.pageYOffset || window.document.documentElement.scrollTop,
+            fold;
+
+        fold = scrollY;
+
+        return fold <= bottom;
+    }
+
+    aboveTheFold (top, height) {
+        var fold,
+            scrollY = window.scrollY || window.pageYOffset || window.document.documentElement.scrollTop;
+
+        top -= 200;
+        fold = this.$window.height() + scrollY;
+
+        return fold >= top;
+    }
+
+    inViewport (top, height) {
+        return this.belowTheFold(top, height) &&
+            this.aboveTheFold(top, height);
+    }
+}
+
+const instance;
+exports default function (element) {
+    element = element || window;
+
+    if (!instance) {
+        instance = new Lazyload();
+    }
+
+    instance.init(element);
+
+    return instance;
+};
--- src/loader.js
+++ src/loader.js
@@ -1,50 +1,54 @@
-define(function (require, exports, module) {
-    'use strict';
-
-    var $ = require('jquery');
-
-    function Loader(options) {
+class Loader {
+    constructor (options) {
         this.options = options;
         this.isLoading = false;
     }
 
-    Loader.prototype = {
-        constructor: Loader,
+    success (element) {
+        var that = this;
+        element.setAttribute('data-lazyload-state', 'complete');
+        that.isLoading = false;
+        that.options.success && that.options.success(element);
+    }
 
-        success: function (element) {
-            var that = this;
-            element.setAttribute('data-lazyload-state', 'complete');
-            that.isLoading = false;
-            that.options.success && that.options.success(element);
-        },
+    fail (element) {
+        var that = this;
+        // pop图片，不再加载
+        that.isLoading = false;
+        element.setAttribute('data-lazyload-state', 'error');
+        that.options.fail && that.options.fail(element);
+    }
 
-        fail: function (element) {
-            var that = this;
-            // pop图片，不再加载
-            that.isLoading = false;
-            element.setAttribute('data-lazyload-state', 'error');
-            that.options.fail && that.options.fail(element);
-        },
+    load (element) {
+        var that = this,
+            state = element.getAttribute('data-lazyload-state'),
+            src = element.getAttribute('data-lazyload-original');
 
-        load: function (element) {
-            var that = this,
-                state = element.getAttribute('data-lazyload-state'),
-                src = element.getAttribute('data-lazyload-original');
-
-            if (state !== 'interactive') {
-                return;
-            }
-
-            this.isLoading = true;
-
-            element.setAttribute('data-lazyload-state', 'loading');
-
-            $(element).one('load', $.proxy(this.success, this, element)).
-                one('error abort', $.proxy(this.fail, this, element));
-
-            element.src = src;
+        if (state !== 'interactive') {
+            return;
         }
-    };
 
-    module.exports = Loader;
-});
+        this.isLoading = true;
+
+        element.setAttribute('data-lazyload-state', 'loading');
+
+        const success = () => {
+            this.success(element);
+            element.removeEventListener('load', success);
+        };
+
+        const fail = () => {
+            this.fail(element);
+            element.removeEventListener('error', fail);
+            element.removeEventListener('abort', fail);
+        };
+
+        element.addEventListener('load', success);
+        element.addEventListener('error', fail);
+        element.addEventListener('abort', fail);
+
+        element.src = src;
+    }
+}
+
+export default Loader;
