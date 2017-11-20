a04b5ae134606657c46f3e79bcef00e88336637b
Hu Jiwei <hu_jiwei@foxmail.com>
2017-11-20 13:28:20 +0800

    update Image api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
-export function Image(props) {
+export function Image({source, placeholder}) {
+    console.log(placeholder);
     return (
         <div
             role="img"
             className="lazyload-img"
-            style={{width: `${props.width}px`}}
-            src={props.src}
+            style={{width: `${source.width}px`}}
+            src={source.uri}
         >
-            <div style={{padding: `${props.height / props.width * 100}% 0 0 0`}}>
-                <img {...props} data-lazyload-state="interactive|loading|complete|error" />
+            <div style={{padding: `${source.height / source.width * 100}% 0 0 0`}}>
+                {
+                    placeholder ?
+                        <img src={placeholder} data-lazyload-state="interactive|loading|complete|error" />
+                        :
+                        <img data-lazyload-state="interactive|loading|complete|error" />
+                }
             </div>
         </div>
     );
 }
+
+Image.propTypes = {
+    source: PropTypes.shape({
+        uri: PropTypes.string.isRequired,
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+    }).isRequired,
+    placeholder: PropTypes.string,
+};
+
+Image.defaultProps = {
+    placeholder: '',
+};
 
 export function lazy(component) {
 }
